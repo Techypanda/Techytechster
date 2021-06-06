39a1509c2002855f7022cb99056ae5adf628a239
Techypanda <Jonathan_wright@hotmail.com>
2021-06-06 22:58:08 +0800

    GithubWorkflow

--- ci/BuildTestDeploy.sh
+++ ci/BuildTestDeploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e # stop on error
-echo "Deploying Infrastructure"
-aws cloudformation deploy --template-file "./Website.yaml" --stack-name Techytechster-WebsiteStack --no-fail-on-empty-changeset --capabilities CAPABILITY_NAMED_IAM
-cd ..
-echo "Infrastructure Deployed Successfully"
+#echo "Deploying Infrastructure"
+#aws cloudformation deploy --template-file "./Website.yaml" --stack-name Techytechster-WebsiteStack --no-fail-on-empty-changeset --capabilities CAPABILITY_NAMED_IAM
+#cd ..
+#echo "Infrastructure Deployed Successfully"
 echo "Building Site"
 yarn install
 yarn build
 cd build
-echo "Deploying To S3"
-aws s3 sync . s3://techytechster
-echo "Finished Build, Test, Deploy"
+#echo "Deploying To S3"
+#aws s3 sync . s3://techytechster DO THIS THROUGH SYNC ACTION ON GITHUB
+echo "Finished Build, Test"
