f56163944ea60315c71624fdb5c3f985dd3a9ccb
Techypanda <Jonathan_wright@hotmail.com>
2021-06-18 19:42:47 +0800

    CORS Change

--- api/wmetric/main.go
+++ api/wmetric/main.go
@@ -15,6 +15,11 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	if err != nil {
 		log.Printf("Failed To Record User: %v", err)
 		return events.APIGatewayProxyResponse{
+			Headers: map[string]string{
+				"Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,X-Amz-Security-Token,Authorization,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
+				"Access-Control-Allow-Origin":  "*",
+				"Access-Control-Allow-Methods": "OPTIONS,POST",
+			},
 			Body:       fmt.Sprintf("Failure to record your visit today: %v", err.Error()),
 			StatusCode: http.StatusBadRequest,
 		}, nil
@@ -23,12 +28,22 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	if err != nil {
 		log.Printf("Failed To Record User: %v", err)
 		return events.APIGatewayProxyResponse{
+			Headers: map[string]string{
+				"Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,X-Amz-Security-Token,Authorization,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
+				"Access-Control-Allow-Origin":  "*",
+				"Access-Control-Allow-Methods": "OPTIONS,POST",
+			},
 			Body:       fmt.Sprintf("Failure to record your visit today: %v", err.Error()),
 			StatusCode: http.StatusBadRequest,
 		}, nil
 	}
 	log.Printf("Successfully Recorded Metric, IPAddress: %v", request.RequestContext.Identity.SourceIP)
 	return events.APIGatewayProxyResponse{
+		Headers: map[string]string{
+			"Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,X-Amz-Security-Token,Authorization,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
+			"Access-Control-Allow-Origin":  "*",
+			"Access-Control-Allow-Methods": "OPTIONS,POST",
+		},
 		Body:       "Hi, I have recorded your visit for website metrics, if you would like to opt out please contact me and I will remove you, (no sensitive data is held its purely for analytics).",
 		StatusCode: http.StatusOK,
 	}, nil
