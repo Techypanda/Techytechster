ce1a83f9e746834526592176ddcfcde637e85c96
Techypanda <Jonathan_wright@hotmail.com>
2021-06-21 20:29:52 +0800

    added env variables and code for redirect

--- adminfrontend/src/pages/Landing.tsx
+++ adminfrontend/src/pages/Landing.tsx
@@ -1,11 +1,26 @@
 import { DefaultProps } from "../interface";
 import styled from 'styled-components';
 import { Container, Typography } from "@material-ui/core";
+import Unauthenticated from "./Unauthenticated";
+import { useQueryClient } from "react-query";
 function Landing(props: DefaultProps) {
+  const client = useQueryClient();
+  function TokenExpired() {
+    if (localStorage.getItem('rToken') && localStorage.getItem('rTokenExpiry')) {
+      try {
+        return !(new Date(new Date().getTime() + (2 * 24 * 60 * 60 * 1000)) < new Date(localStorage.getItem('rTokenExpiry')!)); // if theres atleast 2 days left on token were ok
+      } catch {
+        localStorage.removeItem('rToken');
+        localStorage.removeItem('rTokenExpiry');
+        return true;
+      }
+    }
+    return true;
+  }
+  
   return (
     <Container>
-      <Typography>Landing</Typography>
-      {process.env.REACT_APP_LOGIN}{window.location.href}
+      { TokenExpired() &&  <Unauthenticated /> }
     </Container>
   )
 }
