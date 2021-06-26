c8b23c7ef81dcaad9132dae34c6eb4c8f946e5bf
Techypanda <Jonathan_wright@hotmail.com>
2021-06-26 18:55:47 +0800

    Added processing of payload

--- api/blog/bloglib.go
+++ api/blog/bloglib.go
@@ -1,26 +1,52 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 )
 
-func CreateEndpoint(payload events.APIGatewayProxyRequest) events.APIGatewayProxyResponse {
+type BlogPost struct {
+	Title   string
+	Content string
+	Date    string
+	Author  string
+}
+
+func CreateEndpoint(rawPayload events.APIGatewayProxyRequest) events.APIGatewayProxyResponse {
 	log.Println("Create endpoint begun")
-	return events.APIGatewayProxyResponse{
-		Headers: map[string]string{
-			"Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,X-Amz-Security-Token,Authorization,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
-			"Access-Control-Allow-Origin":  "*",
-			"Access-Control-Allow-Methods": "OPTIONS,POST",
-		},
-		Body:       "Create Endpoint",
-		StatusCode: http.StatusOK,
+	payload, err := processPayload(rawPayload)
+	if err != nil {
+		log.Println("Invalid payload for create endpoint")
+		return events.APIGatewayProxyResponse{
+			Headers: map[string]string{
+				"Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,X-Amz-Security-Token,Authorization,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
+				"Access-Control-Allow-Origin":  "*",
+				"Access-Control-Allow-Methods": "OPTIONS,POST",
+			},
+			Body:       err.Error(),
+			StatusCode: http.StatusBadRequest,
+		}
+	} else {
+		log.Printf("%s, %s, %s", payload.Title, payload.Content, payload.Date)
+		return events.APIGatewayProxyResponse{
+			Headers: map[string]string{
+				"Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,X-Amz-Security-Token,Authorization,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
+				"Access-Control-Allow-Origin":  "*",
+				"Access-Control-Allow-Methods": "OPTIONS,POST",
+			},
+			Body:       "Create Endpoint",
+			StatusCode: http.StatusOK,
+		}
 	}
 }
 
-func DeleteEndpoint(payload events.APIGatewayProxyRequest) events.APIGatewayProxyResponse {
+func DeleteEndpoint(rawPayload events.APIGatewayProxyRequest) events.APIGatewayProxyResponse {
 	log.Println("Delete endpoint begun")
 	return events.APIGatewayProxyResponse{
 		Headers: map[string]string{
@@ -33,7 +59,7 @@ func DeleteEndpoint(payload events.APIGatewayProxyRequest) events.APIGatewayProx
 	}
 }
 
-func UpdateEndpoint(payload events.APIGatewayProxyRequest) events.APIGatewayProxyResponse {
+func UpdateEndpoint(rawPayload events.APIGatewayProxyRequest) events.APIGatewayProxyResponse {
 	log.Println("Update endpoint begun")
 	return events.APIGatewayProxyResponse{
 		Headers: map[string]string{
@@ -44,4 +70,28 @@ func UpdateEndpoint(payload events.APIGatewayProxyRequest) events.APIGatewayProx
 		Body:       "Update Endpoint",
 		StatusCode: http.StatusOK,
 	}
+}
+
+func processPayload(payload events.APIGatewayProxyRequest) (*BlogPost, error) {
+	var post BlogPost
+	if err := json.Unmarshal([]byte(payload.Body), &post); err != nil {
+		return nil, fmt.Errorf("error unmarshling payload: %v", err.Error())
+	}
+	if post.Title == "" {
+		return nil, errors.New("post title is empty in payload")
+	}
+	if post.Content == "" {
+		return nil, errors.New("post content is empty in payload")
+	}
+	if post.Author == "" {
+		return nil, errors.New("post author is empty in payload")
+	}
+	loc, locErr := time.LoadLocation("Australia/Perth")
+	if locErr != nil {
+		return nil, errors.New("error loading australia/perth time")
+	}
+	timeNow := time.Now().In(loc)
+	currentDate := fmt.Sprint(timeNow.Format(time.RFC3339))
+	post.Date = currentDate
+	return &post, nil
 }
