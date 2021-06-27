e4d016f90f3cdd153b47080488fbfe0122d2d32e
Techypanda <Jonathan_wright@hotmail.com>
2021-06-27 13:39:12 +0800

    Create endpoint complete

--- api/blog/bloglib.go
+++ api/blog/bloglib.go
@@ -10,6 +10,9 @@ import (
 	"time"
 
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/lestrrat-go/jwx/jwk"
 )
@@ -19,35 +22,6 @@ type BlogPost struct {
 	Content string
 	Date    string
 	Author  string
-}
-
-const cognitoJWKSURI string = "https://cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_dXWQJmeqM/.well-known/jwks.json"
-
-func validateJWT(accessTokenString string) (bool, error) {
-	keySet, err := jwk.Fetch(context.TODO(), cognitoJWKSURI)
-	_, err = jwt.Parse(accessTokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		}
-		kid, ok := token.Header["kid"].(string)
-		if !ok {
-			return nil, errors.New("kid header missing")
-		}
-		keys, ok := keySet.LookupKeyID(kid)
-		if !ok {
-			return nil, fmt.Errorf("key with specified kid is not present in jwks")
-		}
-		var publickey interface{}
-		err = keys.Raw(&publickey)
-		if err != nil {
-			return nil, fmt.Errorf("could not parse pubkey")
-		}
-		return publickey, nil
-	})
-	if err != nil {
-		return false, err
-	}
-	return true, nil
 }
 
 func CreateEndpoint(rawPayload events.APIGatewayProxyRequest) events.APIGatewayProxyResponse {
@@ -81,14 +55,27 @@ func CreateEndpoint(rawPayload events.APIGatewayProxyRequest) events.APIGatewayP
 			}
 		} else {
 			log.Printf("Creating new post called: %s, created by %s", payload.Title, payload.Author)
-			return events.APIGatewayProxyResponse{
-				Headers: map[string]string{
-					"Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,X-Amz-Security-Token,Authorization,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
-					"Access-Control-Allow-Origin":  "*",
-					"Access-Control-Allow-Methods": "OPTIONS,POST",
-				},
-				Body:       "Create Endpoint",
-				StatusCode: http.StatusOK,
+			err = putBlog(*payload)
+			if err != nil {
+				return events.APIGatewayProxyResponse{
+					Headers: map[string]string{
+						"Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,X-Amz-Security-Token,Authorization,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
+						"Access-Control-Allow-Origin":  "*",
+						"Access-Control-Allow-Methods": "OPTIONS,POST",
+					},
+					Body:       fmt.Sprintf("Failed To Create Blog: %v", err.Error()),
+					StatusCode: http.StatusBadRequest,
+				}
+			} else {
+				return events.APIGatewayProxyResponse{
+					Headers: map[string]string{
+						"Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,X-Amz-Security-Token,Authorization,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
+						"Access-Control-Allow-Origin":  "*",
+						"Access-Control-Allow-Methods": "OPTIONS,POST",
+					},
+					Body:       "Successfully Created New Blog",
+					StatusCode: http.StatusOK,
+				}
 			}
 		}
 	} else {
@@ -153,4 +140,79 @@ func processPayload(payload events.APIGatewayProxyRequest) (*BlogPost, error) {
 	currentDate := fmt.Sprint(timeNow.Format(time.RFC3339))
 	post.Date = currentDate
 	return &post, nil
+}
+
+const cognitoJWKSURI string = "https://cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_dXWQJmeqM/.well-known/jwks.json"
+
+func validateJWT(accessTokenString string) (bool, error) {
+	keySet, err := jwk.Fetch(context.TODO(), cognitoJWKSURI)
+	_, err = jwt.Parse(accessTokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		kid, ok := token.Header["kid"].(string)
+		if !ok {
+			return nil, errors.New("kid header missing")
+		}
+		keys, ok := keySet.LookupKeyID(kid)
+		if !ok {
+			return nil, fmt.Errorf("key with specified kid is not present in jwks")
+		}
+		var publickey interface{}
+		err = keys.Raw(&publickey)
+		if err != nil {
+			return nil, fmt.Errorf("could not parse pubkey")
+		}
+		return publickey, nil
+	})
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func putBlog(post BlogPost) error {
+	newSession := session.Must(session.NewSession())
+	client := dynamodb.New(newSession, aws.NewConfig().WithRegion("ap-southeast-2"))
+	if client != nil {
+		input := &dynamodb.QueryInput{
+			ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+				":title": {
+					S: aws.String(post.Title),
+				},
+			},
+			KeyConditionExpression: aws.String("BlogTitle = :title"),
+			TableName:              aws.String("Techytechster-Blog"),
+		}
+		result, getErr := client.Query(input)
+		if getErr != nil {
+			log.Printf("Failed to create blog: %s", getErr.Error())
+			return getErr
+		}
+		if *result.Count >= 1 {
+			log.Printf("Failed to create blog, it already exists")
+			return errors.New("that blog already exists, please use update endpoint instead")
+		}
+		data := &dynamodb.PutItemInput{
+			Item: map[string]*dynamodb.AttributeValue{
+				"BlogTitle": {
+					S: aws.String(post.Title),
+				},
+				"Date": {
+					S: aws.String(post.Date),
+				},
+				"Content": {
+					S: aws.String(post.Content),
+				},
+				"Author": {
+					S: aws.String(post.Author),
+				},
+			},
+			TableName: aws.String("Techytechster-Blog"),
+		}
+		_, err := client.PutItem(data)
+		return err
+	} else {
+		return errors.New("failed to create dynamodb client")
+	}
 }
--- api/blog/go.mod
+++ api/blog/go.mod
@@ -4,6 +4,8 @@ go 1.16
 
 require (
 	github.com/aws/aws-lambda-go v1.24.0
+	github.com/aws/aws-sdk-go v1.38.68 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/lestrrat-go/jwx v1.2.1 // indirect
 )
