fefe452c02ea42fcc7e34a206b73d8f9a32a15ab
Techypanda <Jonathan_wright@hotmail.com>
2021-07-07 23:38:43 +0800

    No fail on empty changeset and confirm added

--- ci/BuildAPI.sh
+++ ci/BuildAPI.sh
@@ -8,4 +8,4 @@ sam build
 # go test -v
 echo "Beginning Deploy Stage"
 sam package --output-template-file packaged-template.yaml --image-repository 128153249323.dkr.ecr.ap-southeast-2.amazonaws.com/techytechsterapirepo --no-progressbar
-sam deploy --template ./packaged-template.yaml --stack-name techytechsterwebapis --region ap-southeast-2 --capabilities CAPABILITY_IAM --parameter-overrides Authentication="false"
+sam deploy --template ./packaged-template.yaml --stack-name techytechsterwebapis --region ap-southeast-2 --capabilities CAPABILITY_IAM --parameter-overrides Authentication="false" --no-fail-on-empty-changeset --confirm-changeset
