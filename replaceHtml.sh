34585809923b0c6a56be1f2f8bdcde15ad5cb59c
Techypanda <Jonathan_wright@hotmail.com>
2022-06-15 21:56:02 +0800

    please work

--- replaceHtml.sh
+++ replaceHtml.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 for f in build/*.html; do 
-  mv -- "$f" "${f%.html}" # in S3 we dont want them to have file extensions
+  filename="${f%.html}"
+  mv -- "$f" "${f%.html}"
+  key="${filename:6}"
+  aws s3api put-object --bucket techytechster --key $key --content-type text/html --body ./$filename
 done
-mv build/index build/index.html # in s3 we actually do want this one to have a file extension
 echo " 🐵 done cursed solution  🐵"
